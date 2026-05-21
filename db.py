b9913353022d6787385b23bd4767ce88991daae7
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-21 19:32:37 +0530

    after updated

--- db.py
+++ db.py
@@ -41,44 +41,6 @@
 # conn.commit()
 # print("table created successfully")
 import streamlit as st
-import mysql.connector
+from db import conn, cursor
 
-try:
-    conn = mysql.connector.connect(
-        host=st.secrets["MYSQL_HOST"],
-        user=st.secrets["MYSQL_USER"],
-        database=st.secrets["MYSQL_DB"],
-        password=st.secrets["MYSQL_PASSWORD"],
-        port=st.secrets["MYSQL_PORT"],
-        ssl_disabled=False
-    )
-
-    cursor = conn.cursor(dictionary=True)
-
-    cursor.execute("""
-        create table if not exists users2(
-            id int primary key auto_increment,
-            name varchar(50) not null,
-            email varchar(50) unique,
-            password varchar(50) not null
-        )
-    """)
-
-    cursor.execute("""
-        create table if not exists files2(
-            id int primary key auto_increment,
-            user_id int,
-            file_name varchar(255),
-            file_type varchar(100),
-            file_url text,
-            upload_date timestamp default current_timestamp,
-            foreign key(user_id) references users2(id)
-        )
-    """)
-
-    conn.commit()
-
-    st.success("Tables created successfully")
-
-except Exception as e:
-    st.error(e)
+st.title("My App")
