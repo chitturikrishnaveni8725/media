6309ff1de446e633c673b6856ea0ae8a947341bb
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-21 19:56:27 +0530

    after updated

--- db.py
+++ db.py
@@ -1,22 +1,40 @@
+# import streamlit as st
+# import mysql.connector
+
+# conn = None
+# cursor = None
+
+# try:
+#     conn = mysql.connector.connect(
+#         host=st.secrets["MYSQL_HOST"],
+#         user=st.secrets["MYSQL_USER"],
+#         database=st.secrets["MYSQL_DB"],
+#         password=st.secrets["MYSQL_PASSWORD"],
+#         port=st.secrets["MYSQL_PORT"],
+#         ssl_disabled=False
+#     )
+
+#     cursor = conn.cursor(dictionary=True)
+
+#     st.success("Database connected successfully")
+
+# except Exception as e:
+#     st.error(f"ERROR: {e}")
+
+
+
 import streamlit as st
 import mysql.connector
 
-conn = None
-cursor = None
+conn = mysql.connector.connect(
+    host=st.secrets["MYSQL_HOST"],
+    user=st.secrets["MYSQL_USER"],
+    password=st.secrets["MYSQL_PASSWORD"],
+    database=st.secrets["MYSQL_DB"],
+    port=st.secrets["MYSQL_PORT"],
+    ssl_disabled=False
+)
 
-try:
-    conn = mysql.connector.connect(
-        host=st.secrets["MYSQL_HOST"],
-        user=st.secrets["MYSQL_USER"],
-        database=st.secrets["MYSQL_DB"],
-        password=st.secrets["MYSQL_PASSWORD"],
-        port=st.secrets["MYSQL_PORT"],
-        ssl_disabled=False
-    )
+cursor = conn.cursor(dictionary=True)
 
-    cursor = conn.cursor(dictionary=True)
-
-    st.success("Database connected successfully")
-
-except Exception as e:
-    st.error(f"ERROR: {e}")
+st.success("Database Connected")
