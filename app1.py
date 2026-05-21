aacbd7b594e845bc27def342d38f60ad5cb19d65
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-21 19:58:27 +0530

    after updated

--- app1.py
+++ app1.py
@@ -34,5 +34,7 @@ with signup:
         Email=st.text_input("Email")
         password=st.text_input("Password",type="password")
         btn=st.form_submit_button("SignUp")
+        
+st.write("databse connected successfully")
 
 
