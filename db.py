f2950a230688be281450fc393195353d43990e0d
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-21 19:45:17 +0530

    after updated

--- db.py
+++ db.py
@@ -1,42 +1,22 @@
 import streamlit as st
 import mysql.connector
-conn=mysql.connector.connect(
-    host=st.secrets["MYSQL_HOST"],
-    user=st.secrets["MYSQL_USER"],
-    database=st.secrets["MYSQL_DB"],
-    password=st.secrets["MYSQL_PASSWORD"],
-    port=st.secrets["MYSQL_PORT"],
-    ssl_disabled=False
-    
-)
 
+conn = None
+cursor = None
 
-cursor=conn.cursor(dictionary=True)
+try:
+    conn = mysql.connector.connect(
+        host=st.secrets["MYSQL_HOST"],
+        user=st.secrets["MYSQL_USER"],
+        database=st.secrets["MYSQL_DB"],
+        password=st.secrets["MYSQL_PASSWORD"],
+        port=st.secrets["MYSQL_PORT"],
+        ssl_disabled=False
+    )
 
-    
-cursor.execute("""
-               create table if not exists  users2(
-                   id int primary key auto_increment,
-                   name varchar(50) not null,
-                   email varchar(50) unique,
-                   password varchar(50) not null
-               )
-               
-               """)
+    cursor = conn.cursor(dictionary=True)
 
+    st.success("Database connected successfully")
 
-
-cursor.execute("""
-               create table if not exists files2(
-                   id int primary key auto_increment,
-                   user_id int,
-                   file_name varchar(255),
-                   file_type varchar(100),
-                   file_url text,
-                   upload_date timestamp default current_timestamp,
-                   foreign key(user_id) references users2(id)
-               )
-               
-               """)
-conn.commit()
-print("table created successfully")
+except Exception as e:
+    st.error(f"ERROR: {e}")
