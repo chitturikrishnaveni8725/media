950402bcca25bc571b047437e44141a99dbb0e4d
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-05-21 20:01:59 +0530

    after updated

--- app1.py
+++ app1.py
@@ -1,40 +1,50 @@
-# import streamlit as st  
-# from db import conn,cursor
+# # import streamlit as st  
+# # from db import conn,cursor
 
-from db import conn, cursor
-import streamlit as st
+# from db import conn, cursor
+# import streamlit as st
 
-st.title("My App Running Successfully")
-
-
+# st.title("My App Running Successfully")
 
 
-login,signup=st.tabs(
-                ["Login","SignUp"]  
-                )
+
+
+# login,signup=st.tabs(
+#                 ["Login","SignUp"]  
+#                 )
 
         
     
 
-with login:
-    st.header("Login")
-    with st.form("Login_Form"):
-        email=st.text_input("Email")
-        password=st.text_input("Password",type="password")
-        btn=st.form_submit_button("Login")
+# with login:
+#     st.header("Login")
+#     with st.form("Login_Form"):
+#         email=st.text_input("Email")
+#         password=st.text_input("Password",type="password")
+#         btn=st.form_submit_button("Login")
       
      
                 
       
         
-with signup:
-    st.header("SignUp")
-    with st.form("SignUp_form"):
-        name=st.text_input("Name")
-        Email=st.text_input("Email")
-        password=st.text_input("Password",type="password")
-        btn=st.form_submit_button("SignUp")
+# with signup:
+#     st.header("SignUp")
+#     with st.form("SignUp_form"):
+#         name=st.text_input("Name")
+#         Email=st.text_input("Email")
+#         password=st.text_input("Password",type="password")
+#         btn=st.form_submit_button("SignUp")
         
-st.write("databse connected successfully")
+# st.write("databse connected successfully")
 
 
+from db import conn, cursor
+import streamlit as st
+
+st.title("Media Platform")
+
+cursor.execute("SHOW TABLES")
+
+tables = cursor.fetchall()
+
+st.write(tables)
